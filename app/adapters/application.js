d927cdd8e68191cc542c3b85646dae1281ab9b42
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-03 15:33:29 -0400

    fixes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import { underscore } from '@ember/string';
 import Inflector from 'ember-inflector';
-import config from '../../config/environment';
+import config from '../config/environment';
 const inflector = Inflector.inflector;
 export default DS.JSONAPIAdapter.extend({
   // Application specific overrides go here
