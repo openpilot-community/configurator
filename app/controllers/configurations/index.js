724ddddb134bc0efcfcdd8094ce26db1c1f44072
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-10 16:07:01 -0400

    tweaks

--- app/controllers/configurations/index.js
+++ app/controllers/configurations/index.js
@@ -1,18 +1,15 @@
 import Controller from '@ember/controller';
 import EmberObject, { computed, get } from '@ember/object';
 // import { array } from 'ember-awesome-macros';
+import { uniq, mapBy } from '@ember/object/computed';
 
 export default Controller.extend({
   // queryParams: ['make'],
-  makes: computed('model.@each.vehicleMake',function() {
-    const model = get(this,"model");
-
-    if (model) {
-      return model.map((item) => {
-        console.log(item.get('vehicleMake'));
-
-        return item.get("vehicleMake");
-      });
-    }
+  makes: computed(function() {
+    return this.get('store').query('vehicleMake',{
+      filter: {
+        with_configs: true
+      }
+    })
   })
 });
